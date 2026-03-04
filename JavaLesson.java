2542af554bac0993e9e6f83a15f01f47190433a2
mollyroughan <mroughan@bu.edu>
2026-03-04 22:20:00 +0000

    added placesPrint method

--- JavaLesson.java
+++ JavaLesson.java
@@ -1,10 +1,32 @@
 public class JavaLesson {
 
   public static void main(String[] args) {
-    // define arrays of city and state names
-    String[] cities = {"Boston", "Phoenix", "New York City", "Dallas", "Portland"};
-    String[] states = {"MA", "AZ", "NY", "TX", "OR"};
-    String[][] places = {cities, states};
+  
+  String[] cities = {"Boston", "Phoenix", "New York City", "Dallas", "Portland"};
+  String[] states = {"MA", "AZ", "NY", "TX", "OR"};
+  //String[][] places = {cities, states};
 
+  placesPrint(cities);
+  }
+
+  public static void placesPrint (String[] places) {
+    //print out all places
+  for (int i=0; i<5; i++) { //go through 5 steps: i=0,1,2,3,4
+    System.out.println(places[i]);
+  }
+
+  System.out.println();
+
+  //print out last 4 places
+  for (int i=1; i<5; i++) { //goes through i=1,2,3,4
+    System.out.println(places[i]);
+  }
+
+  System.out.println();
+
+  //print out all places
+  for (int i=0; i<=4; i++) { //go through 5 steps: i=0,1,2,3,4
+    System.out.println(places[i]);
+  }
   }
 }
